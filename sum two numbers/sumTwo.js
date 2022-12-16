2f15cf675066c9aa1f8bed92e5bfec243648b395
Ryan <rmh.sturrock@gmail.com>
2022-12-15 20:12:57 -0600

    sum two numbers

---
+++ sum two numbers/sumTwo.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    
+    let indicesSolution = [];
+        nums.forEach(e => {
+            for (let i = nums.indexOf(e) + 1; i < nums.length; i++) {
+                if(nums[i] + e === target && indicesSolution.length < 2) {
+                    indicesSolution.push(nums.indexOf(e),i);
+                    
+                }   
+            }
+        }) 
+        return indicesSolution;
+
+    
+};
+
+console.log(twoSum([3,2,4,3,5,1,2],3))
