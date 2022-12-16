226c1ae7adbfb1cd7564e684f9faa5e68150de6f
Ryan <rmh.sturrock@gmail.com>
2022-12-15 20:43:50 -0600

    added solution to two sums

---
+++ sum two numbers/solution.js
@@ -0,0 +1,13 @@
+// O(n) - One-pass Hash Table
+
+
+var twoSum = function(nums, target) {
+    let map = new Map;
+    for (var i = 0; i < nums.length; i++) {
+        let complement = target - nums[i];
+        if (map.has(complement)) {
+            return [map.get(complement), i]
+        }
+        map.set(nums[i], i);
+    }
+}
